3ac0433770a0c64f7ddbdb38cf68e441f74dbd2d
Cathal Hoare <cathalhoare@gmail.com>
2018-11-11 22:26:11 +0000

    Initial Commit

---
+++ SpannerAppDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpannerAppDemo
+//
+//  Created by Official Things on 11/11/2018.
+//  Copyright © 2018 UCC_CS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
